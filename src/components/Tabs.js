bfc850ff19bee0cedaba4e60a25dc1eaf947c780
Amit chauhan <74815212+Amitjiran@users.noreply.github.com>
2025-01-02 20:43:33 +0530

    Update Tabs.js

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,1 +1,26 @@
 
+import React, { useState } from 'react';
+
+function Tabs({ tabs }) {
+    const [activeTabIndex, setActiveTabIndex] = useState(0); // State to track the active tab
+    return (
+        <div>
+            <div>
+                {tabs.map((tab, index) => (
+                    <button key={index}
+                        onClick={() => setActiveTabIndex(index)}>
+
+                        {tab.title}
+                    </button>
+                ))}
+            </div>
+
+            {/* Render the content of the active tab */}
+            <div>
+                {tabs[activeTabIndex].content}
+            </div>
+        </div>
+    );
+}
+
+export default Tabs;
